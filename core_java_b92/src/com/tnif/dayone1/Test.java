82830d3442b4e41033b17865f4f25aeffc4f5186
Varalakshmisp <varalakshmisp64@gmail.com>
2024-06-19 17:56:48 +0530

    commit2

---
+++ core_java_b92/src/com/tnif/dayone1/Test.java
@@ -0,0 +1,12 @@
+package com.tnif.dayone1;
+
+public class Test {
+
+	public static void main(String[] args) {
+	   System.out.println("Hello World!!");
+	   System.out.print("CNF");
+	   System.out.println("TNS");
+
+	}
+
+}
